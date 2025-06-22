58534f228376a8a821a5437803bdda8777d16422
mikanfactory <k952i4j14x17@gmail.com>
2025-06-22 11:25:23 +0900

    Implement podcast setting form with submission logic

--- bookcast/pages/podcast_setting.py
+++ bookcast/pages/podcast_setting.py
@@ -1,20 +1,26 @@
 import streamlit as st
 
-st.write("podcast setting page")
+from bookcast.page import Rooter
+
+def main():
+    st.write("podcast setting page")
+
+    with st.form("podcast_setting_form"):
+        num_of_people = st.selectbox("人数の選択", options=list(range(1, 3)))
+        personality1 = st.selectbox("話者1の性格を選択", options=["元気", "落ち着き", "クール"])
+        personality2 = st.selectbox("話者2の性格を選択", options=["元気", "落ち着き", "クール"])
+        length_of_podcast = st.number_input(
+            "ポッドキャストの長さ（分）",
+            min_value=1,
+            max_value=15,
+            step=1,
+        )
+        prompt = st.text_area("台本作成用のプロンプト")
+
+        submitted = st.form_submit_button("ポッドキャストを生成開始")
+        if submitted:
+            st.switch_page(Rooter.podcast_page())
 
 
-def callable(*args, **kwargs):
-    print("--------------------------")
-    print(args)
-    print("--------------------------")
-    print(kwargs)
-    print("--------------------------")
-
-
-st.number_input(
-    label="foo",
-    min_value=1,
-    max_value=10,
-    on_change=callable,
-    label_visibility="hidden",
-)
+if __name__ == "__main__":
+    main()
