d31d6d0079e8ad8e00618d5466a392e0307d8878
mikanfactory <k952i4j14x17@gmail.com>
2025-07-06 12:52:05 +0900

    clean

--- app.py
+++ app.py
@@ -1,9 +1,3 @@
-# from bookcast.page import create_page
-#
-# create_page()
+from bookcast.page import create_page
 
-# from bookcast.ocr import main
-# main()
-
-from bookcast.script_writing import main
-main()
+create_page()
