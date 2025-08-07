298a84ab3f59b29bb0e021e4ea167ec845fe8fd9
mikanfactory <k952i4j14x17@gmail.com>
2025-08-07 12:56:26 +0900

    mv frontend dir

---
+++ frontend/src/app.py
@@ -0,0 +1,3 @@
+from bookcast.page import create_page
+
+create_page()
---
+++ frontend/src/pages/podcast.py
@@ -0,0 +1,12 @@
+import streamlit as st
+from streamlit.logger import get_logger
+
+logger = get_logger(__name__)
+
+
+def main():
+    st.write("podcast page")
+
+
+# Execute main function directly for Streamlit
+main()
